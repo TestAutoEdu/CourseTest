08d58c077b1585ecec7990b692a6212bec893300
TestAutoEdu <37870770+TestAutoEdu@users.noreply.github.com>
2018-03-29 14:22:29 +0300

    Тест на сравнение площади квадрата

--- sandbox/src/main/java/marina/edu/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/marina/edu/sandbox/MyFirstProgram.java
@@ -11,12 +11,14 @@ public class MyFirstProgram {
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b + " = " + r.area());
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
     }
+
+
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
+
+
     }
-
-
 }
